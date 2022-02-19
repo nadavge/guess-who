341009af036fa8eea80c8e8bd7aac752b9ec2207
Nadav Geva <nadav.geva4@gmail.com>
2022-02-19 23:21:08 +0200

    Finish game answering logic

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 from copy import deepcopy
 import os
 import random
-from flask import Flask, request
+from flask import Flask, jsonify, request
 import pymongo
 import json
 import bson
@@ -60,9 +60,9 @@ def get_game_info(game_id):
     if game is None:
         return {"error": "Game not found"}, 404
 
-    return {
+    return jsonify({
         "state": game["state"],
-        "questions": game["questions"],
+        "current_question": len(game["questions"])-1 if game["questions"] else None,
         "players": [
             {
                 "id": p["id"],
@@ -71,7 +71,7 @@ def get_game_info(game_id):
             }
             for p in game["players"]
         ]
-    }
+    })
 
 
 @app.route("/game/<game_id>/start", methods=["POST"])
@@ -97,6 +97,20 @@ def start_game(game_id):
     return ""
 
 
+@app.route("/game/<game_id>/question", methods=["GET"])
+def get_questions(game_id):
+    try:
+        game_id = ObjectId(game_id)
+    except bson.errors.InvalidId:
+        return {"error": "Unknown or invalid Game ID"}, 400
+
+    game = db.games.find_one({"_id": game_id})
+    if game is None:
+        return {"error": "Game not found"}, 404
+
+    return jsonify(game["questions"])
+
+
 @app.route("/game/<game_id>/question", methods=["POST"])
 def ask_question(game_id):
     try:
@@ -119,10 +133,29 @@ def ask_question(game_id):
 
     new_question = {
         "title": request.json["title"],
-        "answers": {p["id"]: None for p in game["players"]}
+        "answers": {p["id"]: None for p in game["players"]},
+        "correct_answer": None
     }
 
-    # TODO update the "in/out" state of each player, don't forget there might not be a previous question
+    # Set the status of each player once the new question had been asked
+    if game["questions"]:
+        def new_status(p):
+            if p["game_status"] == "in" and last_question["answers"][p["id"]] == correct_answer:
+                return "in"
+            return "out"
+
+        last_question = game["questions"][-1]
+        correct_answer = last_question["answers"][game["chosen_player_id"]]
+        db.games.update_one(
+            {"_id": game_id},
+            {
+                "$set": {
+                    f"players.{i}.game_status": new_status(p)
+                    for i, p in enumerate(game["players"])
+                }
+            }
+        )
+
     db.games.update_one(
         {"_id": game_id},
         {
@@ -171,19 +204,25 @@ def answer_question(game_id, question_id):
     else:
         return {"error": "Unable to locate user based on secret"}, 400
 
+    # NOTE This can potentially be re-answered after the fact... So be it for now :)
     db.games.update_one(
         {"_id": game_id},
         {
-            "$set": {f"questions.{question_id}.{player['id']}.answer": request.json["answer"]}
+            "$set": {f"questions.{question_id}.answers.{player['id']}": request.json["answer"]}
         }
     )
 
     game = db.games.find_one({"_id": game_id})
     assert game is not None
 
-    # After we answer, we check to see if we were the last answer. We do it after to reduce race conditions
-    if game["state"] == "answer" and all(answer is not None for (_, answer) in game["questions"][question_id]["answers"]):
-        db.games.find_one({"_id": game_id, "state": "answer"}, {"$set": {"state": "ask"}})
+    current_question = game["questions"][question_id]
+    # After we answer, we check to see if we were the last answer. We do it after to _reduce_ race conditions
+    if game["state"] == "answer" and all(answer is not None for answer in current_question["answers"].values()):
+        correct_answer = current_question["answers"][game["chosen_player_id"]]
+        db.games.update_one(
+            {"_id": game_id, "state": "answer"},
+            {"$set": {"state": "ask", f"questions.{question_id}.correct_answer": correct_answer}}
+        )
 
     return "success"
 
@@ -203,12 +242,13 @@ def get_question_info(game_id, question_id):
         return {"error": "Invalid question ID"}, 404
 
     question = game["questions"][question_id]
-    return {
+    return jsonify({
         "title": question["title"],
         "answers": {
-            player_id: answer for (player_id, answer) in question["answers"]
-        }
-    }
+            player_id: answer for (player_id, answer) in question["answers"].items()
+        },
+        "correct_answer": question["correct_answer"]
+    })
 
 
 @ app.route("/game/<game_id>/join", methods=["POST"])
